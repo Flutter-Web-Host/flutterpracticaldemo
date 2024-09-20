f9e392371f7255bc45f657d940934b889eb9f042
mansi <aslaliyamansi28@gmail.com>
2024-09-20 22:32:26 +0530

    Change State management

--- lib/infrastructure/providers/provider_registration.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter_practicle/infrastructure/providers/home_provider.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final homeProvider = ChangeNotifierProvider((ref) => HomeProvider());
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,32 +3,30 @@ import 'package:flutter/material.dart';
 import 'package:flutter_practicle/infrastructure/constants/app_constant.dart';
 import 'package:flutter_practicle/infrastructure/extensions/string_extension.dart';
 import 'package:flutter_practicle/infrastructure/providers/home_provider.dart';
-import 'package:flutter_practicle/infrastructure/providers/provider_registration.dart';
 import 'package:flutter_practicle/screens/product_details_screen.dart';
 import 'package:flutter_practicle/screens/widgets/category_tile.dart';
 import 'package:flutter_practicle/screens/widgets/product_tile.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:provider/provider.dart';
 
-class HomeScreen extends ConsumerStatefulWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
-  ConsumerState<HomeScreen> createState() => _HomeScreenState();
+  State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends ConsumerState<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen> {
   @override
   void initState() {
     WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
-      ref.read(homeProvider).getProductDataApi();
+      context.read<HomeProvider>().getProductDataApi();
     });
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    final homeRead = ref.read(homeProvider);
-    final homeWatch = ref.watch(homeProvider);
+    final homeRead = context.read<HomeProvider>();
 
     return Scaffold(
       appBar: AppBar(
@@ -42,7 +40,7 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
           IconButton(
             icon: const Icon(Icons.filter_list),
             onPressed: () {
-              _showFilterDialog(context, homeWatch, () {
+              _showFilterDialog(context, () {
                 Navigator.pop(context);
                 homeRead.filterProductsByMultipleBrands();
               });
@@ -50,139 +48,148 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
           ),
         ],
       ),
-      body: homeWatch.isLoading ?? false
-          ? const Center(
+      body: Consumer<HomeProvider>(
+        builder: (BuildContext context, value, Widget? child) {
+          if (value.isLoading ?? false) {
+            return const Center(
               child: CupertinoActivityIndicator(
                 color: Colors.deepPurple,
               ),
-            )
-          : SingleChildScrollView(
-              child: Column(
-                children: [
-                  // Horizontal scrollable categories
-                  homeWatch.categoriesList.isNotEmpty
-                      ? SizedBox(
-                          height: 30,
-                          child: ListView.builder(
-                            scrollDirection: Axis.horizontal,
-                            itemCount: homeWatch.categoriesList.length,
-                            itemBuilder: (context, index) {
-                              return CategoryButton(
-                                  label:
-                                      (homeWatch.categoriesList[index].name ??
-                                              '')
-                                          .capitalize(),
-                                  isSelected: homeWatch.selectedCategories
-                                      .contains(
-                                          homeWatch.categoriesList[index].name),
-                                  onPressed: () => homeRead
-                                      .filterProductsByMultipleCategory(index));
-                            },
-                          ),
-                        )
-                      : const SizedBox.shrink(),
+            );
+          }
+          return SingleChildScrollView(
+            child: Column(
+              children: [
+                // Horizontal scrollable categories
+                value.categoriesList.isNotEmpty
+                    ? SizedBox(
+                        height: 30,
+                        child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: value.categoriesList.length,
+                          itemBuilder: (context, index) {
+                            return CategoryButton(
+                                label: (value.categoriesList[index].name ?? '')
+                                    .capitalize(),
+                                isSelected: value.selectedCategories
+                                    .contains(value.categoriesList[index].name),
+                                onPressed: () => homeRead
+                                    .filterProductsByMultipleCategory(index));
+                          },
+                        ),
+                      )
+                    : const SizedBox.shrink(),
 
-                  const SizedBox(height: 10),
-                  // Vertical scrollable products
-                  homeWatch.isListLoading ?? false
-                      ? SizedBox(
-                          height: MediaQuery.of(context).size.height / 1.2,
-                          child: const CupertinoActivityIndicator(
-                            color: Colors.deepPurple,
-                          ),
-                        )
-                      : homeWatch.productListData.isNotEmpty
-                          ? SizedBox(
-                              height: MediaQuery.of(context).size.height / 1.2,
-                              child: ListView.separated(
-                                  itemBuilder: (context, index) {
-                                    final data =
-                                        homeWatch.productListData[index];
-                                    return GestureDetector(
-                                      onTap: () {
-                                        Navigator.push(
-                                          context,
-                                          MaterialPageRoute(
-                                            builder: (context) =>
-                                                ProductDetailScreen(
-                                              productName: data.title ?? '',
-                                              image: data.images?.first ?? '',
-                                              description:
-                                                  data.description ?? '',
-                                              reviews: data.reviews ?? [],
-                                            ),
-                                          ),
-                                        );
-                                      },
-                                      child: ProductCard(
+                const SizedBox(height: 10),
+                // Vertical scrollable products
+                Consumer<HomeProvider>(
+                  builder: (BuildContext context, HomeProvider value,
+                      Widget? child) {
+                    if (value.isListLoading ?? false) {
+                      return SizedBox(
+                        height: MediaQuery.of(context).size.height / 1.2,
+                        child: const CupertinoActivityIndicator(
+                          color: Colors.deepPurple,
+                        ),
+                      );
+                    }
+                    if (value.productListData.isNotEmpty) {
+                      return SizedBox(
+                        height: MediaQuery.of(context).size.height / 1.2,
+                        child: ListView.separated(
+                            itemBuilder: (context, index) {
+                              final data = value.productListData[index];
+                              return GestureDetector(
+                                onTap: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => ProductDetailScreen(
                                         productName: data.title ?? '',
-                                        brandName: data.brand ?? '',
-                                        imageUrl: data.images?.first ?? '',
-                                        discount:
-                                            data.discountPercentage.toString(),
-                                        price: data.price.toString(),
+                                        image: data.images?.first ?? '',
+                                        description: data.description ?? '',
+                                        reviews: data.reviews ?? [],
                                       ),
-                                    );
-                                  },
-                                  separatorBuilder: (context, index) =>
-                                      const SizedBox(height: 10),
-                                  itemCount: homeWatch.productListData.length),
-                            )
-                          : const SizedBox.shrink(),
-                ],
-              ),
+                                    ),
+                                  );
+                                },
+                                child: ProductCard(
+                                  productName: data.title ?? '',
+                                  brandName: data.brand ?? '',
+                                  imageUrl: data.images?.first ?? '',
+                                  discount: data.discountPercentage.toString(),
+                                  price: data.price.toString(),
+                                ),
+                              );
+                            },
+                            separatorBuilder: (context, index) =>
+                                const SizedBox(height: 10),
+                            itemCount: value.productListData.length),
+                      );
+                    } else {
+                      return const SizedBox.shrink();
+                    }
+                  },
+                )
+              ],
             ),
+          );
+        },
+      ),
     );
   }
 
 // Filter Dialog for selecting brands
-  void _showFilterDialog(
-      BuildContext context, HomeProvider homeWatch, VoidCallback onApplyTap) {
+  void _showFilterDialog(BuildContext context, VoidCallback onApplyTap) {
     showDialog(
       context: context,
       builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text(
-            AppConstant.selectBrand,
-            style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
-          ),
-          content: StatefulBuilder(
-            builder: (BuildContext context, StateSetter setState) {
-              return SingleChildScrollView(
-                child: Column(
-                  children: List.generate(
-                    homeWatch.brandList.length,
-                    (index) => CheckboxListTile(
-                      title: Text(homeWatch.brandList[index].name ?? ''),
-                      value: homeWatch.selectedBrands
-                          .contains(homeWatch.brandList[index].name),
-                      onChanged: (bool? value) {
-                        setState(() {
-                          if (value == true) {
-                            homeWatch.selectedBrands
-                                .add(homeWatch.brandList[index].name ?? '');
-                          } else {
-                            homeWatch.selectedBrands
-                                .remove(homeWatch.brandList[index].name ?? '');
-                          }
-                        });
-                      },
+        return Consumer<HomeProvider>(
+          builder: (BuildContext context, value, Widget? child) {
+            return AlertDialog(
+              title: const Text(
+                AppConstant.selectBrand,
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+              ),
+              content: StatefulBuilder(
+                builder: (BuildContext context, StateSetter setState) {
+                  return SingleChildScrollView(
+                    child: Column(
+                      children: List.generate(
+                        value.brandList.length,
+                        (index) => CheckboxListTile(
+                          title: Text(value.brandList[index].name ?? ''),
+                          value: value.selectedBrands
+                              .contains(value.brandList[index].name),
+                          onChanged: (bool? data) {
+                            setState(() {
+                              if (data == true) {
+                                value.selectedBrands
+                                    .add(value.brandList[index].name ?? '');
+                              } else {
+                                value.selectedBrands
+                                    .remove(value.brandList[index].name ?? '');
+                              }
+                            });
+                          },
+                        ),
+                      ),
                     ),
-                  ),
+                  );
+                },
+              ),
+              actions: [
+                TextButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: const Text(AppConstant.cancel),
                 ),
-              );
-            },
-          ),
-          actions: [
-            TextButton(
-              onPressed: () => Navigator.pop(context),
-              child: const Text(AppConstant.cancel),
-            ),
-            TextButton(
-              onPressed: onApplyTap,
-              child: const Text(AppConstant.apply),
-            ),
-          ],
+                TextButton(
+                  onPressed: onApplyTap,
+                  child: const Text(AppConstant.apply),
+                ),
+              ],
+            );
+          },
         );
       },
     );
